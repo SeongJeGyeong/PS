d6ec0de29aec8d9e44d8ec07bf41f8a54125ea87
SeongJeGyeong <143048374+SeongJeGyeong@users.noreply.github.com>
2026-01-17 23:42:35 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    int lux = 51;
+    int luy = 51; 
+    int rdx = -1;
+    int rdy = -1;
+    
+    for(int i = 0; i < wallpaper.size(); ++i)
+    {
+        for(int j = 0; j < wallpaper[i].size(); ++j)
+        {
+            if(wallpaper[i][j] == '.') continue;
+            lux = min(lux, i);
+            luy = min(luy, j);
+            rdx = max(rdx, i+1);
+            rdy = max(rdy, j+1);
+            
+        }
+    }
+    answer.push_back(lux);
+    answer.push_back(luy);
+    answer.push_back(rdx);
+    answer.push_back(rdy);
+    return answer;
+}
