21d78c75f9e6a6d9ebb990629a9db5b8e4ec5b5f
SeongJeGyeong <143048374+SeongJeGyeong@users.noreply.github.com>
2026-04-15 00:46:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -2,20 +2,13 @@
 
 using namespace std;
 
-bool cmp(int a, int b)
-{
-    if (a < b) return !(a % 2);
-
-    return !(b % 2);
-}
-
 int solution(int n, int a, int b)
 {
-    int answer = 1;
-    while(abs(a - b) > 1 || cmp(a, b))
+    int answer = 0;
+    while(a != b)
     {
-        a = (a % 2) ? a / 2 + 1 : a / 2;
-        b = (b % 2) ? b / 2 + 1 : b / 2;
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
         ++answer;
     }
     
